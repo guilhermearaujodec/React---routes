8f7eb93af552e8e416182e12c0ea2d0da938d59a
Guilherme <gui_araujo.carvalho@hotmail.com>
2024-08-30 09:57:18 -0300

    Montando rotas e componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+//Importando as dependências para criar as rotas
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Aluno from './routes/Aluno.jsx'
+
+
+// CRIANDO A FUNÇÃO createBrowserRouter
+const router = createBrowserRouter([
+  {
+    // CRIANDO AS ROTAS PAI
+    path:'/',element:<App/>,
+    errorElement:<Error/>,
+
+
+    // CRIANDO ROTAS FILHAS
+    children:[
+      {path:'/',element:<Home/>},
+      {path:'/',element:<Aluno/>}
+    ]
+  }
+])
+
+// () - Método
+// [] - Array
+// {} - Objeto
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* CRIANDO A DESESTRUCT ROUTER PARA SER RENDERIZADO */}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
