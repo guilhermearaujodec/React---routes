ae6ba5ab02bb81da12b118d9a61250d6255d2ab1
Guilherme <gui_araujo.carvalho@hotmail.com>
2024-08-30 08:58:16 -0300

    Criando projeto exemplo2 react+vite

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer=()=>{
+    return(
+        <>
+        <h1>@2024-Todos os direitos reservados</h1>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/routes/Error.jsx
@@ -0,0 +1,8 @@
+const Error=()=>{
+    return(
+        <>
+        <h1>404 - Page Not Found !!! - Se Liga e Digita Direito!!!</h1>
+        </>
+    )
+}
+export default Error
